a6b20271db7813d054a055bb14c5a1d8036e00e7
Damian Brzezinski <damian.brzezinski@ppuc.pl>
2022-04-12 21:31:36 +0200

    added errors handling to login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  form!: FormGroup
 
-  constructor() { }
+  constructor(private formBuild: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.formBuild.group({
+      name: this.formBuild.control('', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(25)
+      ]),
+      password: this.formBuild.control('', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(25)
+      ]
+      )
+    })
+  }
+
+  sendData(){
+
   }
 
 }
