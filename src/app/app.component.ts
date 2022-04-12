7bce04c7e5e49f801bbf5a889c231b016ff8f0b8
Damian Brzezinski <damian.brzezinski@ppuc.pl>
2022-04-12 11:55:00 +0200

    Initial commit damian

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log("Initial commit - Damian");
+  }
 }
