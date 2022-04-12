cb45fdcf13f149dd9fad76efbc48c705617b849e
Damian Brzezinski <damian.brzezinski@ppuc.pl>
2022-04-12 15:50:05 +0200

    created navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { LogoutComponent } from './logout/logout.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, LogoutComponent],
+  declarations: [AppComponent, NavbarComponent, LogoutComponent, HeaderComponent, LogoComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
